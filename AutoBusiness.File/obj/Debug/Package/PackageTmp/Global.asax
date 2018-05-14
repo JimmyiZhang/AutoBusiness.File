23af96740327c864237d132edbedfabc1c59062d
zhangjianmin@kuaicheyuan.com <zhangjianmin@kuaicheyuan.com>
2018-05-14 09:36:30 +0800

    提交

---
+++ AutoBusiness.File/obj/Debug/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AutoBusiness.File.WebApiApplication" Language="C#" %>
---
+++ Autobusiness.File.Build/Debug/Scripts/jquery.fileupload/image-base64.js
@@ -0,0 +1,29 @@
+﻿; (function (window) {
+    'use strict'
+
+    var imagebase64 = function (file, width, height, callback) {
+        var reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = function (e) {
+            var img = new Image;
+            img.src = this.result;
+
+            var canvas = document.createElement("canvas");
+            var drawer = canvas.getContext("2d");
+
+            if (height > width) {
+                canvas.width = width;
+                canvas.height = width * (img.height / img.width);
+            } else {
+                canvas.height = height;
+                canvas.width = height * (img.width / img.height);
+            }
+
+            drawer.drawImage(img, 0, 0, canvas.width, canvas.height);
+            var dataURL = canvas.toDataURL('image/png');
+            callback(dataURL);
+        }
+    }
+
+    window.imagebase64 = imagebase64;
+}(window))
